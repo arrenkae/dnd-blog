083899270e012ddc93ff547946b4a59c90ea47a4
slayerstokely <stokely@slayers.ru>
2024-07-17 20:36:12 +0300

    modified pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,4 @@
-let tpl = <h1 className="mt-20 ml-20">Hello World</h1>;
+let tpl = <h1 className="text-center mt-20">Hello World</h1>;
 
 export default function BlogPage() {
   return tpl;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-center mt-20">
       <Link href="/blog">Blog</Link>
     </div>
   );
